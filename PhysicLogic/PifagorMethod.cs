a71b57fdae910d4cf0edd66cd69bdeb6a4075aa7
Igor228337 <Dj.igo@yandex.ru>
2024-02-22 12:57:34 +0300

    Added UI

---
+++ PhysicLogic/PifagorMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5.PhysicLogic
+{
+    internal class PifagorMethod
+    {
+        static double CalculateHypotenuse(int a, int b, int angel)
+        {
+            double c = Math.Pow(a, 2) + Math.Pow(b, 2) - (2 * b * a) * Math.Cos(angel);
+            return Math.Sqrt(c);
+        }
+    }
+}
